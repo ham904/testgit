b55f4799c5815aa7478a78cade0db74a21eb2ec2
ham904 <ham904904@gmail.com>
2023-08-18 06:59:53 +0900

    [update] Hello.java

--- Begins/Hello/src/hello/Hello.java
+++ Begins/Hello/src/hello/Hello.java
@@ -1,10 +1,15 @@
 package hello;
 
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이것이 자바다");
+		String[] weeks = { "월", "화", "수", "목", "금", "토", "일" };
+		for (int i = 0; i < weeks.length; i++) {
+			System.out.println(weeks[i]);
+		}
 	}
 
 }
